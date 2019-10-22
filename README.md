0776ca6ef6a1de25e4be8bf652a34ef442841e91
Geordie Rogers <geordie@grogers.xyz>
2019-10-22 19:11:04 +1300

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# table-view
-View DynamoDB tables in the command line
+# Table View
+
+View simple DynamoDB tables from the command line
+
+## Instillation
+
+1. Install the requirements using `pip install -r requirements.txt`
+2. Run one of the commands below
+
+
+## Examples
+
+### List Tables `./table-view.py -ls`
+
+```text
+$ ./table-view.py -ls
++ org_accounts
++ users
++ groups
+```
+
+### View Table `./table-view.py <table_name>`
+
+```text
+$ ./table-view.py org_accounts
++-----+-------+--------------+--------------+
+| age | owner |  account_id  |     name     |
++-----+-------+--------------+--------------+
+|  1  |  Liam | 123456789012 |  Production  |
+|     |       | 733456499012 | Staging-Blue |
++-----+-------+--------------+--------------+
+```
+
+### Help Text `./table-view.py --help`
+
+```text
+usage: Table View [-h] [-ls] [--profile PROFILE] [--region REGION] [-v]
+                  [dynamo_table]
+
+positional arguments:
+  dynamo_table       the DynamoDB table name or ID
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -ls                list all of the tables
+  --profile PROFILE  the AWS profile to use
+  --region REGION    the AWS region the instance is in
+  -v, --verbose      increase output verbosity
+```
---
+++ table-view.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import boto3
+import argparse
+import pprint
+from prettytable import PrettyTable
+from functools import reduce
+
+
+def list_tables(client):
+    tables = client.list_tables()['TableNames']
+    for table_name in tables:
+        print(f'+ {table_name}')
+
+
+def display_table(client, table_name):
+    items = client.scan(TableName=table_name)['Items']
+    table = PrettyTable()
+    headers = []
+    for item in items:
+        headers.extend(item.keys())
+    headers = list(set(headers))
+    table.field_names = headers
+    for item in items:
+        row = [''] * len(headers)
+        for key in item.keys():
+            row[headers.index(key)] = item[key][next(iter(item[key]))]
+        table.add_row(row)
+    print(table)
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='Table View')
+    parser.add_argument(
+        'table', nargs='?', help='the DynamoDB table name or ID', metavar='dynamo_table')
+    parser.add_argument('-ls', action='store_true',
+                        help='list all of the tables', )
+    parser.add_argument('--profile', help='the AWS profile to use', type=str)
+    parser.add_argument('--region', help='the AWS region the instance is in',
+                        type=str, default='ap-southeast-2')
+    parser.add_argument("-v", "--verbose",
+                        help="increase output verbosity", action="store_true")
+
+    args = parser.parse_args()
+
+    client = boto3.session.Session(
+        region_name=args.region, profile_name=args.profile).client('dynamodb')
+
+    if args.ls:
+        list_tables(client)
+    elif args.table:
+        display_table(client, args.table)
+    else:
+        print("\nPlease provide either a table name or use -ls to list all tables\n")
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()

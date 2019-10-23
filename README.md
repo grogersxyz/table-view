2e9558a782e9e333d6583f8acde456c489ffec84
Geordie Rogers <geordie@grogers.xyz>
2019-10-23 17:19:58 +1300

    Add sorting to table view

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ View simple DynamoDB tables from the command line
 1. Install the requirements using `pip install -r requirements.txt`
 2. Run one of the commands below
 
-
 ## Examples
 
 ### List Tables `./table-view.py -ls`
@@ -21,20 +20,36 @@ $ ./table-view.py -ls
 
 ### View Table `./table-view.py <table_name>`
 
+Columns are ordered alphabetically
+
 ```text
 $ ./table-view.py org_accounts
-+-----+-------+--------------+--------------+
-| age | owner |  account_id  |     name     |
-+-----+-------+--------------+--------------+
-|  1  |  Liam | 123456789012 |  Production  |
-|     |       | 733456499012 | Staging-Blue |
-+-----+-------+--------------+--------------+
++--------------+-----+--------------+-------+
+|  account_id  | age |     name     | owner |
++--------------+-----+--------------+-------+
+| 123456789012 |  1  |  Production  |  Liam |
+| 733456499012 |     | Staging-Blue |       |
++--------------+-----+--------------+-------+
+```
+
+You can sort by a column name as well by passing the column name to `--sort`.
+By default it will sort in `--ascending/--asc` order, you can also sort by `--descending/--des` order.
+
+```text
+$ ./table-view.py org_accounts --sort name --des
++--------------+-----+--------------+-------+
+|  account_id  | age |     name     | owner |
++--------------+-----+--------------+-------+
+| 733456499012 |     | Staging-Blue |       |
+| 123456789012 |  1  |  Production  |  Liam |
++--------------+-----+--------------+-------+
 ```
 
 ### Help Text `./table-view.py --help`
 
 ```text
-usage: Table View [-h] [-ls] [--profile PROFILE] [--region REGION] [-v]
+usage: Table View [-h] [-ls] [--profile PROFILE] [--region REGION] [--verbose]
+                  [--sort SORT] [--ascending | --descending]
                   [dynamo_table]
 
 positional arguments:
@@ -45,5 +60,8 @@ optional arguments:
   -ls                list all of the tables
   --profile PROFILE  the AWS profile to use
   --region REGION    the AWS region the instance is in
-  -v, --verbose      increase output verbosity
+  --verbose, -v      increase output verbosity
+  --sort SORT        the column to sort entries by
+  --ascending        sort in an ascending order
+  --descending       sort in a decending order
 ```
--- table-view.py
+++ table-view.py
@@ -9,22 +9,28 @@ from functools import reduce
 def list_tables(client):
     tables = client.list_tables()['TableNames']
     for table_name in tables:
-        print(f'+ {table_name}')
+        print('+ {}'.format(table_name))
 
 
-def display_table(client, table_name):
+def display_table(client, table_name, sort_column, desc):
     items = client.scan(TableName=table_name)['Items']
     table = PrettyTable()
     headers = []
     for item in items:
         headers.extend(item.keys())
     headers = list(set(headers))
+    headers.sort()
     table.field_names = headers
     for item in items:
         row = [''] * len(headers)
         for key in item.keys():
             row[headers.index(key)] = item[key][next(iter(item[key]))]
         table.add_row(row)
+
+    if sort_column:
+        table.sortby = sort_column
+        table.reversesort = desc
+
     print(table)
 
 
@@ -37,8 +43,14 @@ def main():
     parser.add_argument('--profile', help='the AWS profile to use', type=str)
     parser.add_argument('--region', help='the AWS region the instance is in',
                         type=str, default='ap-southeast-2')
-    parser.add_argument("-v", "--verbose",
-                        help="increase output verbosity", action="store_true")
+    parser.add_argument('--verbose', '-v',
+                        help='increase output verbosity', action='store_true')
+    parser.add_argument('--sort', help='the column to sort entries by')
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('--ascending', action='store_true',
+                       help='sort in an ascending order')
+    group.add_argument('--descending', action='store_true',
+                       help='sort in a decending order')
 
     args = parser.parse_args()
 
@@ -48,11 +60,11 @@ def main():
     if args.ls:
         list_tables(client)
     elif args.table:
-        display_table(client, args.table)
+        display_table(client, args.table, args.sort, args.descending)
     else:
-        print("\nPlease provide either a table name or use -ls to list all tables\n")
+        print('\nPlease provide either a table name or use -ls to list all tables\n')
         parser.print_help()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
